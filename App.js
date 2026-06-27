fc7cdaac971cda4975e89aeccad745c7c26cdd23
TsvetelinIliev <bho981@gmail.com>
2026-06-27 18:35:54 +0100

    Add main sections

--- App.js
+++ App.js
@@ -2,19 +2,48 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            {/* Header */}
+            <View>
+
+                <Text>Callorie Tracker</Text>
+
+
+            </View>
+
+            {/* Overview */}
+            <View>
+
+                <Text>Callorie Overview</Text>
+
+            </View>
+
+            {/* Meal section */}
+
+            <View>
+
+                <Text>List of meals</Text>
+
+            </View>
+
+            {/* App Bar */}
+
+            <View>
+
+                <Text>App Bar</Text>
+
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
 });
