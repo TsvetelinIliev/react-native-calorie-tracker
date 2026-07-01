a8a2438dd73d6b0ac5634cc74484c393230905da
TsvetelinIliev <bho981@gmail.com>
2026-07-01 19:09:18 +0100

    Add meal modal

--- components/add-meal/AddMeal.jsx
+++ components/add-meal/AddMeal.jsx
@@ -1,4 +1,4 @@
-import { Modal, Text } from "react-native";
+import { Button, Modal, StyleSheet, Text,TextInput,View } from "react-native";
 
 
 export default function AddMeal({
@@ -10,10 +10,56 @@ export default function AddMeal({
         <Modal 
         visible= {visible}
         onRequestClose={onClose}
+        transparent
 
         
         >
-            <Text>Add meal</Text>
+            <View style={ styles.overlay} >
+                
+                <View style={styles.modal} >
+
+                    <Text>Add Meal</Text>
+
+                    <View>
+                        <Text>Food Name</Text>
+                        <TextInput placeholder="e.g. Pizza slice" />
+
+                    </View>
+
+                    <View>
+                        <Text>Calories</Text>
+                        <TextInput  placeholder="0"  keyboardType="number-pad" />
+                    </View>
+
+                    <View>
+                        {/* <Button  title="Back"/>
+                        <Button  title="Add"/> */}
+                    </View>
+                </View>
+
+            </View>
         </Modal>
     )
 }
+
+const styles = StyleSheet.create({
+    overlay: {
+
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        alignItems: 'center',
+
+
+    },
+    modal: {
+
+        width: 300,
+        height: 200,
+        backgroundColor:'white',
+        borderRadius: 12,
+        padding: 16,
+
+
+    }
+});
