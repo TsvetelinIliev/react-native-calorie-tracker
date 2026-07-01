aab27b460fb227f8fe4bbf5595842fca1f44303a
TsvetelinIliev <bho981@gmail.com>
2026-07-01 15:34:13 +0100

    Add section styles

--- App.js
+++ App.js
@@ -8,15 +8,15 @@ export default function App() {
         <SafeAreaProvider>
         <SafeAreaView style={styles.container}>
             {/* Header */}
-            <View>
+            <View style={[styles.section, styles.header]} >
 
-                <Text>Callorie Tracker</Text>
+                <Text style={styles.heading} >Callorie Tracker</Text>
 
 
             </View>
 
             {/* Overview */}
-            <View>
+            <View style={styles.section} >
 
                 <Text>Callorie Overview</Text>
 
@@ -24,7 +24,7 @@ export default function App() {
 
             {/* Meal section */}
 
-            <View>
+            <View style={styles.section}>
 
                 <Text>List of meals</Text>
 
@@ -32,22 +32,53 @@ export default function App() {
 
             {/* App Bar */}
 
-            <View>
+            <View style={styles.section}>
 
                 <Text>App Bar</Text>
 
             </View>
+
             <StatusBar style="auto" />
         </SafeAreaView>
         </SafeAreaProvider>
     );
+};
+
+const colors = {
+    white: '#fff',
+    background: '#F9FAFB',
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: colors.background,
         alignItems: 'center',
         justifyContent: 'space-between',
     },
+    section: {
+
+         //boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+        boxShadow: [{offsetX: 2, offsetY: 3, blurRadius: 4,color: 'rgba(200,200,200,0.53)' }],
+        backgroundColor: colors.white,
+        borderWidth: 1,
+        borderColor: '#edf2f7',
+        borderRadius: 15,
+        width: '90%',
+        padding: 10,
+
+
+    },
+    header: {
+
+    },
+    heading: {
+
+        textAlign: 'center',
+        fontSize: 26,
+        fontWeight: 'bold',
+
+
+
+    }
 });
